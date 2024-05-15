d3d1e628fa8edf9f4f51d0fa6e8bafc4a19ed638
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-05-15 16:51:34 -0300

    Update struct.c

---
+++ struct.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct carStruct 
+{ 
+    char car_name[15];
+    char car_model[10];
+    int car_horsepw;
+    int car_fuelPerMile;
+}
+
+int main() 
+{
+    
+    
+    
+    return 0:
+}
