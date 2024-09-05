1ca44b98171d3a4835b5c90baa7e91d5a0fcc673
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-09-05 03:32:19 -0300

    v1.3

--- array/string.c
+++ array/string.c
@@ -5,9 +5,9 @@ int main()
 {
     char userName[15];
     printf("Insert a name: ");
-    scanf("%s", &userName);
+    scanf("%s", userName);
 
-    for (int i = 0; i > strlen(userName); i++)
+    for (int i = 0; i < strlen(userName); i++)
     {
         printf("%c", &userName[i]);
     }
