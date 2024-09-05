dfed4c6a19e986ef42de291d17c791038e601760
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-09-05 18:00:00 -0300

    v1.4

--- array/upperCase.c
+++ array/upperCase.c
@@ -4,8 +4,9 @@
 int main(void)
 {
     char userText[20];
-    printf("Before:\n");
-    scanf("%s", userText);
+
+    printf("Before:");
+    scanf("%19s", userText);
     printf("After: ");
 
     for (int i = 0, n = strlen(userText); i < n; i++)
@@ -13,18 +14,11 @@ int main(void)
         if (userText[i] >= 'a' && userText[i] <= 'z')
         {
             printf("%c", userText[i] - 32);
+            
         } else {
-            printf("%c", userText[i]);
-        }
-    printf("\n");
-
-    return 0;
-}
-            // Mantenha outros caracteres inalterados
             printf("%c", userText[i]);
         }
     }
     printf("\n");
-
     return 0;
 }
