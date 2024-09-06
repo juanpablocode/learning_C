cfaee1acdf76470ee92683f36167f4bc03d13bbf
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-09-06 17:10:24 -0300

    v1.0

---
+++ array/greet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    /*char answer[15];
+    printf("Input:\n");
+    scanf("%s", answer);*/
+    printf("print %s \n", argv[1]);
+    
+    return 0;
+}
