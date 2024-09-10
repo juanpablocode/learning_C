5098f0cdc8f1450fbffc537fadc66cbfc68fa59d
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-09-10 18:06:29 -0300

    v2

--- array/alphabet.c
+++ array/alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int main (int argv, char* argc)
+int main (int argc, char* argv[])
 {
-    char alphabet[11] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', '\0'};
+    char alphabet[11] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', '7', 'J', '\0'};
   
   //printf("Insert the alphabet: \n");
   //scanf("%s", alphabet);
     printf("Alphabet Verificator \n");
     for (int i = 0, count = strlen(alphabet); i < count; i++)
         {
-            if (alphabet[i] >= "A" && alphabet[i] <="Z" )
+            if (alphabet[i] >= 'A' && alphabet[i] <= 'Z' )
                 {
                     printf("%c ", alphabet[i]);
                 } else {
