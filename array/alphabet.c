c6fdb9d50d17b18b76033eb88194b607aabeafc5
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-09-10 01:22:30 -0300

    v1.0

---
+++ array/alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (int argv, char* argc)
+{
+  char alphabet[27];
+  int count;
+  
+  printf("Insert the alphabet: \n");
+  scanf("%s", alphabet);
+  count = strlen(alphabet);
+
+  for (int i = 0; i < count; i++)
+    {
+      if (alphabet[i} >= "A" && alphabet[i] <="Z" )
+    }
+  
+  return 0:
+}
