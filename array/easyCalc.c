1a91b9269cffb12a59fe5e796a47f2f7351b6a4a
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-09-12 17:31:31 -0300

    v1.2
    Versão estável

--- array/easyCalc.c
+++ array/easyCalc.c
@@ -2,11 +2,10 @@
 #include <string.h>
 
 int main(int argc, char* argv[])
-{
-
-    for (int i = 0 ; i < argc; i++)
+{   
+    for (int i = 0; i < argc; i++)
     {
-        printf("argv[%i] is %s \n", argv[i]);
+        printf("argv[%i] is %s \n", i, argv[i]);
     }
     return 0;
 }
