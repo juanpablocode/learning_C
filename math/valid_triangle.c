e8f6df77c1655e53e1e25ff5855140f2d2fba146
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-09-24 11:41:45 -0300

    v1.1
    adiçao de virificação de triângulo valido

--- math/valid_triangle.c
+++ math/valid_triangle.c
@@ -3,13 +3,31 @@
 int triangle_verificator(int x, int y, int z);
 int main()
 {
-    int x, y, z;
+    int x, y, z, aws;
     printf("Insert the First side of the tryangle: \n");
     scanf("%5d", &x);
     printf("Insert the second side: \n");
     scanf("%5d", &y);
-    printf("Insert the third side");   
+    printf("Insert the third side\n");   
     scanf("%5d", &z);
-    printf("It's a valid triangle, and its area is %d \n", (x+y+z));
+    
+    aws = triangle_verificator(x, y, z);
+
+    if (aws > 0)
+    {
+        printf("This is a valid triangle, its sides are %d %d %d \n", x, y, z);
+    } else{
+        printf("This is not a valid tringle, its sides are %d %d %d\n", x, y, z);
+    }
     return 0;
+}
+
+int triangle_verificator(int x, int y, int z)
+{
+    if (x + y  > z && x + z = y && z + y = x)
+    {
+        return 0;
+    } else{
+        return 1;
+    }
 }
