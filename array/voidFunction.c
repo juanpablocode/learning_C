fde6ecf2ba99f4792bcb2ec27a5d713bf9576290
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-10-11 00:03:45 -0300

    v1.0

---
+++ array/voidFunction.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void add_int();
+void add_array();
+
+int main()
+{
+    int a = 10;
+    int b[4] = {0, 1, 2, 3};
+    
+    add_int(a);
+    add_array(b);
+
+    printf("%d, %d \n", a, b[0]);
+    return 0;
+}
+
+void add_int (int x)
+{
+    x = 22;
+}
+
+void add_array(int array[4])
+{
+    int array[0] = 22;
+}
