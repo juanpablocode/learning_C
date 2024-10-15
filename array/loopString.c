e319442f99649b944d573d9be75b26516c9d6ba5
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-10-15 15:28:03 -0300

    v1.2

--- array/loopString.c
+++ array/loopString.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
-{
+int main(void) {
     char word_list[6][12] = {"cat", "word", "charmander", "cellphone", "school", "pride"};
-    int count = sizeof(word_list); 
-    for (int i = 0; i < count; i++)
-    {
-        printf("%c\n", word_list[i]);
-    }
-    
+    int count = sizeof(word_list) / sizeof(word_list[0]);
 
+    for (int i = 0; i < count; i++) {
+        if (strchr(word_list[i], 's') != NULL) {
+            printf("Founded\n");
+            return 0;
+        }
+    }
     return 0;
 }
