cfd396abaa7aee3f70fd922ced10a894dda36907
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-10-18 16:32:06 -0300

    v1.2

--- array/sovedNumberArray.c
+++ array/sovedNumberArray.c
@@ -3,19 +3,37 @@
 
 int main(void)
 {
-    int ramdom_num[10] = {10, 8, 7, 4, 1, 3, 2, 6, 9, 5};
-    int solved_num[10];
+    int ramdom_num[10] = {8, 10, 7, 4, 1, 3, 2, 6, 9, 5};
+    int solved_num[10] = {0};
     int count = sizeof(ramdom_num) / 4;
+    int holder, place, p, j = 0;
+    while (j < count)
+    {
+    
+        for (int i = 0; i < count; i++)
+        {
+            if (i == 0)
+            {
+                holder = ramdom_num[j];
+            }
+            if (holder < ramdom_num[i])
+            {
+                p++;
+            }
+
+        }
+        place = (count - p) - 1;
+        solved_num[place] = holder;
+        p = 0;
+        j++;
+
+    }
     
     for (int i = 0; i < count; i++)
     {
-        printf("Working %d\n", i);
+        printf("%d ", solved_num[i]);
     }
-
-    for (int i = 0; i < count; i++)
-    {
-        printf("%d, ", solved_num[i]);
-    }
+    printf("\n");
     
     
     return 0;
