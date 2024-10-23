78ff294c6de02a4d3ef49fd2de3b19dcf1d1112e
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-10-22 23:36:44 -0300

    v1.0

---
+++ ex/election.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct 
+{
+  char name[25];
+  char party[10];
+  int votes;
+}
+candidate;
+
+int main (void)
+{
+  int count;
+
+  candidate president;
+  president.name;
+  president.party;
+  president.votes;
+
+  return 0;
+}
