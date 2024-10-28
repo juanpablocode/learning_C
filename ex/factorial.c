5f93b278de3f539fadf8021c69dd44ad2eefae16
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-10-28 16:50:05 -0300

    1.0
    versão inicial

---
+++ ex/factorial.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    int fac_num, count = 0;
+    fac_num = argv[1];
+    count = fac_num;
+    int num_size[count];
+
+    if(argv[1] == 0)
+    {
+        printf("Factorial of 0! = 1\n");
+    }
+    if(argv[1] == 1)
+    {
+        printf("Factorial of 1! = 1\n");
+    }
+    for(int i = count; i < 0; i--)
+        {
+            num_size[i] = fac_num - i;
+        }
+
+    printf("Factorial of %d! = ");
+    for(int i = 0; i < count; i++)
+        {
+            printf("%d ", num_size[i]);
+            if( i + 1 < count)
+            { 
+                printf(".\n")
+            }else{
+              printf("x ");
+            }
+        }
+    return 0;
+}
