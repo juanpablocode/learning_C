8389d1c689dc4d30393c715a750fe44907f9232c
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-11-26 02:45:50 -0300

    less bugs

--- ex/fun_factorial.c
+++ ex/fun_factorial.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fun_looking();
+int fun_looking(int user_num, int num[], int *times);
 
 int main(void)
 {
     char user_input[8];
-    int num[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int user_num, founded, times, count = 0;
+    int num[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int user_num, founded = 0, times = 0;
 
     printf("Insert a input for looking:\n");
     scanf("%2s", user_input);
     user_num = atoi(user_input);
 
-    fun_looking(user_num, num, times);
+    founded = fun_looking(user_num, num, &times);
 
-    if (founded = 1)
+    if (founded = 0)
     {
         printf("The value %d was founded after %d attemps\n", user_num, times);
     }else {
@@ -27,17 +27,18 @@ int main(void)
     return 0;
 }
 
-int fun_looking(int user_num, int num_func, int times)
+int fun_looking(int user_num, int num[], int *times)
 {
-    int counter = sizeof(num_func)/4 ;
+    int counter = sizeof(num) / sizeof(num[0]);
     for (int i = 0; i < counter; i++)
     {
-        if (num_func = user_num)
+        if (num[i] == user_num)
         {
             printf("Founded!\n");
+            return 0;
         } else{
-            times++;
+            *times++;
         }
     }
-    return 0;
+    return 1;
 }
