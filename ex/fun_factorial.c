fac0cbe172db8b295e0f6fd22450584603e3a5f4
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-12-01 22:45:49 -0300

    v1.6

--- ex/fun_factorial.c
+++ ex/fun_factorial.c
@@ -1,44 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fun_looking(int user_num, int num[], int *times, int count);
+int fun_search(int num[], int count, int user_num, int *times, int *founded);
 
 int main(void)
 {
-    char user_input[8];
+    char user_input[4];
     int num[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int user_num, founded = 0, times = 0;
+    int user_num, founded, times = 0;
 
-    printf("Insert a input for looking:\n");
+    printf("Insert the num for searching:\n");
     scanf("%2s", user_input);
     user_num = atoi(user_input);
 
-    int count = sizeof(num) / sizeof(num[0]);
-    founded = fun_looking(user_num, num, &times, count);
+    int count = sizeof(num)/sizeof(num[0]);
+    fun_search(num, count, user_num, &times, &founded);
 
-    if (founded == 0)
+    if(founded == 0)
     {
         printf("The value %d was founded after %d attemps\n", user_num, times);
-    }else {
+        
+    }else{
         printf("The value %d wasn't founded\n", user_num);
-        printf("Tried for %d times\n", times);
-        return 1;
+        printf("We tried for %d times.\n", times);
     }
-    
+
     return 0;
 }
 
-int fun_looking(int user_num, int num[], int *times, int count)
+int fun_search(int num[], int count, int user_num, int *times, int *founded)
 {
     for (int i = 0; i < count; i++)
     {
+        (*times)++;
         if (num[i] == user_num)
         {
-            printf("Founded!\n");
+            printf("Founded! \n");
+            (*founded) = 0;
             return 0;
-        } else{
-            (*times)++;
         }
     }
-    return 1;
+    (*founded) = 1;
+    return 0;
 }
