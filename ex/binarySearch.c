f6730ea379cf2ac460efbdc1b237797cad7c7d16
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-12-14 16:21:16 -0300

    commit inicial

---
+++ ex/binarySearch.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stlib.h>
+
+int binarySrc();
+
+int main(void)
+{
+  char user_input[3];
+  int user_choice, count, times = 0;
+  int arrayMain[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int arrayRight, arrayLeft;
+  return 0;
+}
