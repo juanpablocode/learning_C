88c7a5a46b63a44c34e6b85fbac9ea576e3a774d
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-12-17 01:02:44 -0300

    Update binarySearch.c

--- ex/binarySearch.c
+++ ex/binarySearch.c
@@ -5,9 +5,37 @@ int binarySrc();
 
 int main(void)
 {
-  char user_input[3];
-  int user_choice, count, times = 0;
-  int arrayMain[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int arrayRight, arrayLeft;
+    char user_input[3];
+    int user_choice, count, times = 0;
+    int arrayMain[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arrayRight[10], arrayLeft[10];
+  
+    count = sizeof(arrayMain)/sizeof(arrayMain[0]);
+    
+    deskArray(count);
+    
+    scanf("%2s", user_input);
+    printf("What number you are looking for\n");
+    user_choice = atoi(user_input);
+  
+    return 0;
+}
+
+int deskArray()
+{
+    printf("Your List\n");
+    printf("( \n");
+    for(i = 0; i < count; i++);
+        {
+          printf("%d "arrayMain[i]);
+        }
+    printf(" )\n");
+  
+    return 0;
+}
+
+int binarySrc()
+{
+  
   return 0;
 }
