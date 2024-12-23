0b8082bff0a5200f95412bba77ba226044b90ca3
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-12-23 10:02:51 -0300

    update binary function

--- ex/binarySearch.c
+++ ex/binarySearch.c
@@ -5,8 +5,6 @@ int deskArray(int count, int arrayMain[]);
 
 int banarySrc(int arrayMain[], int user_choice, int length, int *times);
 
-//int getArrSize(int arrayMain[], int arraySize[], int user_choice);
-
 int main(void)
 {
     char user_input[3];
@@ -42,20 +40,29 @@ int deskArray(int count, int arrayMain[])
 
 int banarySrc(int arrayMain[], int user_choice, int length, int *times)
 {
-    //int arraySize[10];
-    //getArrSize(arrayMain, arraySize, user_choice);
     int arrayRight[10], arrayLeft[10];
-    int middle = (length / 2);
+    int middle = (length / 2) -1;
+    (*times)++;
     
-    if (user_choice == arrayMain[middle])
+    if (user_choice == (arrayMain[middle]))
     {
-        printf("Founded %d !/n after 1 attempts\n", user_choice);
-    } else if(user_choice < middle)
+        printf("Founded %d !/n after %d attempts\n", user_choice, *times);
+        return 0;
+
+    }
+    if(user_choice < middle)
     {
         for (int i = 0; i < middle; i++)
         {
             arrayLeft[i] = arrayMain[i];    
         }
+        if(user_choice == (middle / 2))
+        {
+            (*times)++;
+            printf("Founded %d!\n After %d Attempts\n", user_choice, *times);
+            return 0;
+        }
+
     } else if (user_choice > middle)
     {
         for (int i = length; i > middle; i--)
@@ -65,17 +72,5 @@ int banarySrc(int arrayMain[], int user_choice, int length, int *times)
 
     }
 
-    /*for (int i = middle; i < 0; i--)
-    {
-        if (arrayLeft[i] == user_choice)
-        {
-            printf("Founded %d!\n", user_choice);
-            return 0;
-        }
-        
-    }*/
-
-    return 0;
+    return 1;
 }
-
-/*int getArrSize(int arrayMain[], int arraySize[], int user_choice)
