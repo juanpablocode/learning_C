c6d46f8d8392deed3ec01a9e1a6419296f2a0b96
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2024-12-30 13:56:07 -0300

    add new functions

--- ex/bubbleSort.c
+++ ex/bubbleSort.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void describle(int arrMain[], int size);
+int bubbleSort (int arrMain[], int *times);
+
 int main(void)
 {
     int arrMain[10]= {5, 7, 3, 1, 9, 4, 2, 6, 0, 8};
-    int userChoice, times = 0;
+    int times = 0;
+
+    int size = sizeof(arrMain) / sizeof(arrMain[0]);
 
     printf("Before:\n");
-    descible(arrMain);
+    describle(arrMain, size);
 
     printf("After: \n");
-    int OnOff = bubbleShort(arrMain, &times);
+    int OnOff = bubbleSort(arrMain, &times);
+    return 0;
+}
 
+void describle(int arrMain[], int size)
+{
+    printf("( ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arrMain[i]);
+    }
+    printf(")\n");
+}
+
+int bubbleSort (int arrMain[], int *times)
+{
     
     return 0;
 }
