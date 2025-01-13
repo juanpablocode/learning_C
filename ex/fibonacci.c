2fb0ca626ce63cac16d1178e2d611efb29de446f
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2025-01-13 19:46:49 -0300

    finifhed fibonacci.c

--- ex/fibonacci.c
+++ ex/fibonacci.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
-int sequence(int *n, int *times);
+int sequence(int n);
 
 int main(void)
 {   
-    int *times, *n = 0;
+    int times, n = 0;
 
-    sequence(&n, &times);
+    sequence(n);
 
     return 0;
 }
 
-int sequence(int *n, int *times)
+int sequence(int n)
 {   
-    (*n = n + n++;)
-    if (times < 15)
+    if ( n == 1)
     {
         return 0;
+    }else if((n % 2) == 0)
+    {
+        return 1 + sequence(n/2);
+    }else {
+        return 1 + sequence(3*n + 1);
     }
-    sequence(*n, *times )
+
 }
