0675abc15d95b1777a9ad6e4dddbcd91f514551f
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2025-02-19 13:49:03 -0300

    add file variables

--- files_pointer/accessPdf.c
+++ files_pointer/accessPdf.c
@@ -2,8 +2,17 @@
 #include <stdint.h>
 
 int main(int argc, int* argv[])
-{
-    
+{   
+    char filename[20] = argv[1];    
+    FILE *archive = fopen(filename, "r");
+    uint8_t buffer[4];
+    fread(buffer, 1, 4, archive);
 
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%i \n", buffer[i]);    
+    }
+    fclose(archive)
+    
     return 0;
 }
