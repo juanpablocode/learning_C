531bf3a3c1d0a49ed8fe2721a26dea0b97a21d45
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2025-02-23 06:57:32 -0300

    add security verifications

--- files_pointer/accessPdf.c
+++ files_pointer/accessPdf.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main(int argc, int* argv[])
+int main(int argc, char* argv[])
 {   
-    char filename[20] = argv[1];    
-    FILE *archive = fopen(filename, "r");
+    if (argc != 2)
+    {
+        printf("Usage %s <filename>\n", argv[0]);
+        return 1;
+    }
+    
+    const char* filename[] = argv[1];    
+    FILE *archive = fopen(*filename, "r");
+    if (archive == NULL)
+    {
+        printf("Unable to open file %s\n", filename);
+        return 1;
+    }
+    
     uint8_t buffer[4];
     fread(buffer, 1, 4, archive);
 
     for (int i = 0; i < 4; i++)
     {
-        printf("%i \n", buffer[i]);    
+        printf("%i \n", buffer[i]);
     }
-    fclose(archive)
+
+    fclose(archive);
     
     return 0;
 }
