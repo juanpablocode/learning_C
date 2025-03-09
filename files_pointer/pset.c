442a4735e914121fd45b94c698fc355854c2ffd9
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2025-03-09 09:28:49 -0300

    Update pset.c

--- files_pointer/pset.c
+++ files_pointer/pset.c
@@ -4,34 +4,55 @@ int main(void)
 {
     int Score = 0;
     int* pScore = &Score;
-    int Heart = 5;
+    int Heart = 3;
     int* pHeart = &Heart;
-    int choice, n1 = 1;
+    int endGame, n1 = 1;
 
-    while (*pHeart < 0)
+    while (*pHeart >= 0 || endGame <= 1)
     {
         printf("How much is 50 + 50 ?\n");
         scanf("%i", &n1);
-        if (n1 = (50 + 50))
+        if (n1 == (50 + 50))
         {
-            *pScore = 10;
+            *pScore += 10;
+            printf("Correct!\n");
         }else{
-            *pScore = -10;
-            *pHeart = -1;
+            printf("Incorrect!\n");
+            *pScore -= 10;
+            *pHeart -= 1;
         }
         
         printf("How much is 13 + 5 ?\n");
         scanf("%i", &n1);
-        if (n1 = (13 + 5))
+        if (n1 == (13 + 5))
         {
-            *pScore = 10;
+            *pScore += 10;
             printf("Correct!\n");
         }else{
-            *pScore = -10;
-            *pHeart = -1;
+            printf("Incorrect!\n");
+            *pScore -= 10;
+            *pHeart -= 1;
         }
-    }
-    
-    
 
+        printf("How much is 55 + 5 ?\n");
+        scanf("%i", &n1);
+        if (n1 == (55 + 5))
+        {
+            *pScore += 10;
+            printf("Correct!\n");
+        }else{
+            printf("Incorrect!\n");
+            *pScore -= 10;
+            *pHeart += 1;
+        }
+
+        printf("Would you like to try again? \n");
+        printf("1 - No; 2 - Yes; \n");
+        scanf("%d", &endGame);
+        
+    }
+    printf("Endgame!\n");
+    printf("Your final score was %d \n", *pScore);
+    printf("Hearts left: %d\n", *pHeart);
+    
 }
